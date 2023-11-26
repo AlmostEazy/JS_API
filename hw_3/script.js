3da2bd5ea959f2beee836d5e927a26aed70bac4c
AlmostEazy <Antosha101992@rambler.ru>
2023-11-27 00:01:47 +0300

    feat: add hw_3 files

---
+++ hw_3/script.js
@@ -0,0 +1,49 @@
+window.addEventListener('load', () => {
+    renderPhoto();
+});
+
+async function getRandomPhoto() {
+    const apiKey = 'qrAPbcLieJ2JU9Ne3OCqawHaVEnQg63ZELYotPpYsIo';
+    try {
+        const response = await fetch(`https://api.unsplash.com/photos/random?client_id=${apiKey}`);
+        const photo = await response.json();
+        return photo;
+    } catch (error) {
+        console.error('Ошибка при загрузке фотографий:', error);
+        return {};
+    }
+}
+
+async function renderPhoto() {
+    const photo = await getRandomPhoto();
+    if (photo) {
+        const imageBox = document.querySelector('.image_container');
+        imageBox.addEventListener('click', function() {
+            location.reload();
+        })
+        const img = document.createElement('img');
+        img.classList.add('image');
+
+        img.src = photo.urls.small;
+        img.alt = photo.alt_description;
+        imageBox.appendChild(img);
+
+        const imagePhotographerNameDiv = document.querySelector('.photographer_info_name');
+        imagePhotographerNameDiv.textContent = `${photo.user.name}`;
+
+        const imageLikesCounterSpan = document.querySelector('.image_likes_counter');
+        imageLikesCounterSpan.textContent = `${photo.likes}`;
+
+    }
+}
+
+const counterButton = document.querySelector('.image_likes_button');
+counterButton.addEventListener('click', function () {
+    increaseCounter();
+});
+
+function increaseCounter() {
+    const likesCounter = document.querySelector('.image_likes_counter');
+    const currentCounter = parseInt(likesCounter.textContent, 10);
+    likesCounter.textContent = currentCounter + 1;
+}
